e4da7d9420fe867666c590b1abc576bd51ed8b84
jean <jeandesir84@gmail.com>
2020-12-10 02:08:39 +0100

    adsf

--- API-connect/config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['all' => true],
-    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['all' => true],
-];
